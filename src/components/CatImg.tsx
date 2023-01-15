72bc4a759e68a0ae05005524d1e87459f4e565c8
Anastasiya <iinokayo@mail.ru>
2023-01-15 14:21:57 +0300

    Cats API broken

--- src/components/CatImg.tsx
+++ src/components/CatImg.tsx
@@ -3,35 +3,61 @@ import CatLoader from './CatLoader';
 
 function CatImg() {
   const [img, setImg] = useState<Blob>();
+  const [imgUrl, setImgUrl] = useState('img/gag.jpg');
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    setIsLoading(false);
-  }, []);
+  const catImg = document.querySelector<HTMLImageElement>('.file_img');
+
+  // useEffect(() => {
+  //   setIsLoading(false);
+  // }, []);
 
   const getCatImg = async () => {
     setIsLoading(true);
-    setImg(undefined);
+    // setImgUrl('');
+    // setImg(undefined);
 
-    const url = 'https://cataas.com/cat';
-    const response = await fetch(url);
-    const newImg: Blob = await response.blob();
-    setImg(newImg);
+    if (catImg) {
+      catImg.style.opacity = '0';
+    }
+
+    // const urlFile = 'https://cataas.com/cat';
+    // const response = await fetch(urlFile);
+    // const newImg: Blob = await response.blob();
+    // setImg(newImg);
+
+    const urlData = 'https://api.thecatapi.com/v1/images/search';
+    const responseData = await fetch(urlData);
+    const [data] = await responseData.json();
+
+    setImgUrl(data.url);
   };
 
   const handleImageLoading = () => {
     setIsLoading(false);
+
+    if (catImg) {
+      catImg.style.opacity = '1';
+    }
   };
 
   return (
     <div className="file">
       <div className="file_item">
         {isLoading && <CatLoader />}
-        {img && (
+        {/* {img && (
           <img
             className="file_img"
             onLoad={handleImageLoading}
             src={URL.createObjectURL(img)}
+            alt="cat-img"
+          />
+        )} */}
+        {imgUrl && (
+          <img
+            className="file_img"
+            onLoad={handleImageLoading}
+            src={imgUrl}
             alt="cat-img"
           />
         )}
