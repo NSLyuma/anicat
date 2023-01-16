39547e3cbb6fa304f9e51aa575607a105610ca11
Anastasiya <iinokayo@mail.ru>
2023-01-16 14:23:49 +0300

    Added versions for broken API

--- src/components/CatGif.tsx
+++ src/components/CatGif.tsx
@@ -45,3 +45,61 @@ function CatGif() {
 }
 
 export default CatGif;
+
+// ------------------------------------------------
+// при поломке API закомментить сверху и раскомментить снизу
+// ------------------------------------------------
+
+// import { useState } from 'react';
+// import CatLoader from './CatLoader';
+
+// const getRandom = () => {
+//   return Math.floor(Math.random() * (16 - 1) + 1);
+// };
+
+// function CatGif() {
+//   const [gif, setGif] = useState('/img/cats/cat1.gif');
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   const gifEl = document.querySelector<HTMLImageElement>('.broken-gif');
+
+//   const getCatGif = () => {
+//     setIsLoading(true);
+
+//     if (gifEl) {
+//       gifEl.style.opacity = '0';
+//     }
+
+//     setGif(`/img/cats/cat${getRandom()}.gif`);
+//   };
+
+//   const handleImageLoading = () => {
+//     setIsLoading(false);
+
+//     if (gifEl) {
+//       gifEl.style.opacity = '1';
+//     }
+//   };
+
+//   return (
+//     <div className="file">
+//       <div className="file_item">
+//         {isLoading && <CatLoader />}
+//         {gif && (
+//           <img
+//             className="file_img broken-gif"
+//             onLoad={handleImageLoading}
+//             src={gif}
+//             alt="cat-gif"
+//           />
+//         )}
+//       </div>
+
+//       <button className="file_btn" onClick={getCatGif}>
+//         Хочу гифку с котом!
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default CatGif;
--- src/components/CatImg.tsx
+++ src/components/CatImg.tsx
@@ -13,8 +13,8 @@ function CatImg() {
     setIsLoading(true);
     setImg(undefined);
 
-    const urlFile = 'https://cataas.com/cat';
-    const response = await fetch(urlFile);
+    const url = 'https://cataas.com/cat';
+    const response = await fetch(url);
     const newImg: Blob = await response.blob();
     setImg(newImg);
   };
@@ -45,3 +45,61 @@ function CatImg() {
 }
 
 export default CatImg;
+
+// ------------------------------------------------
+// при поломке API закомментить сверху и раскомментить снизу
+// ------------------------------------------------
+
+// import { useState } from 'react';
+// import CatLoader from './CatLoader';
+
+// function CatImg() {
+//   const [imgUrl, setImgUrl] = useState('img/gag.jpg');
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   const imgEl = document.querySelector<HTMLImageElement>('.broken-img');
+
+//   const getCatImg = async () => {
+//     setIsLoading(true);
+
+//     if (imgEl) {
+//       imgEl.style.opacity = '0';
+//     }
+
+//     const url = 'https://api.thecatapi.com/v1/images/search';
+//     const response = await fetch(url);
+//     const [data] = await response.json();
+
+//     setImgUrl(data.url);
+//   };
+
+//   const handleImageLoading = () => {
+//     setIsLoading(false);
+
+//     if (imgEl) {
+//       imgEl.style.opacity = '1';
+//     }
+//   };
+
+//   return (
+//     <div className="file">
+//       <div className="file_item">
+//         {isLoading && <CatLoader />}
+//         {imgUrl && (
+//           <img
+//             className="file_img broken-img"
+//             onLoad={handleImageLoading}
+//             src={imgUrl}
+//             alt="cat-img"
+//           />
+//         )}
+//       </div>
+
+//       <button className="file_btn" onClick={getCatImg}>
+//         Хочу картинку с котом!
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default CatImg;
